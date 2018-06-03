3d71b18e03f40b389e520c758deb5a4534857e1e
Justin Richter <justin.richter.99@gmail.com>
2018-06-03 02:23:20 -0400

    Project Stage 1 Completed

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        private ComicBookRepository _comicBookRespository = null;
+        private ComicBookRepository _comicBookRepository = null;
 
         public ComicBooksController()
         {
-            _comicBookRespository = new ComicBookRepository();
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
         }
         
         public ActionResult Detail(int? id)  //Base class is shared Result Type for ContentResult and RedirectResult
@@ -24,7 +31,7 @@ namespace ComicBookGallery.Controllers
                 return HttpNotFound();
             }
 
-            var comicBook = _comicBookRespository.GetComicBook((int)id);
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
 
             return View(comicBook);
         }
